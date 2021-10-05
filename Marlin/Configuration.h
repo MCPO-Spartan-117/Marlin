2724cf7de5c8390e5e873f64620819faad030b90
chief <chief@maddmerlin.com>
2021-10-05 02:53:30 -0400

    Мастер: Custom UUID

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -147,7 +147,7 @@
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
-//#define MACHINE_UUID "00000000-0000-0000-0000-000000000000"
+#define MACHINE_UUID "8c770e3d-2502-402c-a536-4b02c221e6e6"
 
 /**
  * Define the number of coordinated linear axes.
